41a8a43897ae06560ba221099ef437d6e01d02c9
Aurela Broqi <elabroqi@gmail.com>
2026-05-25 20:51:51 -0400

    heart rate

--- ring_client/read_ring.py
+++ ring_client/read_ring.py
@@ -2,7 +2,7 @@ import asyncio
 from datetime import datetime, timezone
 
 from colmi_r02_client.client import Client
-
+from colmi_r02_client import real_time
 
 RING_ADDRESS = "31:30:45:32:E9:06"
 
@@ -14,14 +14,35 @@ async def main():
         await ring.set_time(datetime.now(timezone.utc))
         print("Ring time synced")
 
-        battery = await ring.get_battery()
-        print("Battery:", battery)
+        # BEFORE ACTIVITY
+        start_steps_data = await ring.get_steps(datetime.now(timezone.utc))
+        print("Starting Steps:", start_steps_data)
 
-        device_info = await ring.get_device_info()
-        print("Device Info:", device_info)
+        input("Walk around, then press ENTER to continue...")
 
-        steps = await ring.get_steps(datetime.now(timezone.utc))
-        print("Steps:", steps)
+        # AFTER ACTIVITY
+        end_steps_data = await ring.get_steps(datetime.now(timezone.utc))
+        print("Ending Steps:", end_steps_data)
+
+        # REAL-TIME HEART RATE
+        hr_values = await ring.get_realtime_reading(
+            real_time.RealTimeReading.HEART_RATE
+        )
+        print("Heart rate:", hr_values)
+
+        # EXAMPLE STEP DELTA CALCULATION
+        if (
+            isinstance(start_steps_data, list)
+            and isinstance(end_steps_data, list)
+            and len(start_steps_data) > 0
+            and len(end_steps_data) > 0
+        ):
+            start_steps = start_steps_data[-1].steps
+            end_steps = end_steps_data[-1].steps
+
+            session_steps = end_steps - start_steps
+
+            print("Session Steps:", session_steps)
 
 
 if __name__ == "__main__":
