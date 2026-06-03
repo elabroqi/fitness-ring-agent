059ee1a3cecb4cdf9a6bc990df98d12f59bc8c57
Aurela Broqi <152095314+elabroqi@users.noreply.github.com>
2026-06-03 19:22:41 -0400

    adding ios

---
+++ ios/FitnessRingAgent/FitnessRingAgent/FitnessRingAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessRingAgentApp.swift
+//  FitnessRingAgent
+//
+//  Created by Temp User on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct FitnessRingAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
